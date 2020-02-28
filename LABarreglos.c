3c982bc63d9ce259ebadfd276b860b976a2f4805
Emily Soto <emilysoto@ufm.edu>
2020-02-27 21:39:23 -0600

    Probando

--- LABarreglos.c
+++ LABarreglos.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-int main(void){
-    int arrg[5]={1,2,3,4,5};
+
+int give_me_numbers(void){
+    int number;
+
+    printf("How big is your array? (Must be less than 25)\n");
+    scanf("%d", &number);
+
+    int give_me[number];
+
+
+    for(int i =0;i < number; i++){
+        printf("Give me number %d: ", i +1);
+        scanf("%d", &give_me[i]);
+    }
+
+    for(int i = 0; i < number; i++){
+        printf("%d ",give_me[i]);
+    }
+
+
     return 0;
 }
 
+
+int main(void){
+    give_me_numbers();
+    return 0; 
+
+}
