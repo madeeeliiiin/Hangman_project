9127561ae9ec55830a7cf668c2cf1e7c0d1e8d40
Emily Soto <emilysoto@ufm.edu>
2020-03-04 19:59:04 -0600

    Inicio

---
+++ ahorcado.c
@@ -0,0 +1,15 @@
+/*Proyecto Juego de ahorcado
+    Emily Soto 
+    Madeline Salguero
+*/
+
+//librerias
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<stdlib.h>
+
+int main(void){
+    return 0;
+}
